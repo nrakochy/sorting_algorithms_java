b702e199253ee6a43762387deb0610e77c345311
nrakochy <nick.rakochy@gmail.com>
2014-12-11 15:10:09 -0600

    Refactored Insertion sort method. Extracted 3 private methods to simplify readability.

--- src/com/company/InsertionSort.java
+++ src/com/company/InsertionSort.java
@@ -9,28 +9,43 @@ import java.util.List;
 public class InsertionSort {
     public List sort(Integer[] unsortedArray) {
         List<Integer> sortedList = new ArrayList<Integer>();
-        Integer unsortedArrayLength = unsortedArray.length;
 
-        for(Integer unsortedIndex = 0; unsortedIndex < unsortedArrayLength; unsortedIndex++){
+        for (Integer unsortedIndex = 0; unsortedIndex < unsortedArray.length; unsortedIndex++) {
             Integer unsortedElement = unsortedArray[unsortedIndex];
-
-            if (sortedList.isEmpty()) {
-                sortedList.add(unsortedElement);
+            if(sortedList.isEmpty()) {
+                addElementToEmptySortedList(sortedList, unsortedElement);
             } else {
-                Integer sortedListSize = sortedList.size();
-                for(Integer sortedIndex = 0; sortedIndex < sortedListSize; sortedIndex++){
-                    if(unsortedElement <= sortedList.get(sortedIndex)){
-                        sortedList.add(sortedIndex, unsortedElement);
-                        sortedIndex = sortedListSize;
-                    }
-                }
-                if(unsortedElement > sortedList.get(sortedListSize-1)){
-                    sortedList.add(unsortedElement);
-                }
+                addElementToListIfSmallerThanHighestSortedInteger(sortedList, unsortedElement);
+                addElementToListIfHigherThanBiggestSortedInteger(sortedList, unsortedElement);
+            }
+        }
+    return sortedList;
+    }
+
+    private List addElementToEmptySortedList(List<Integer> sortedList, Integer unsortedElement) {
+        sortedList.add(unsortedElement);
+        return sortedList;
+    }
+
+    private List addElementToListIfSmallerThanHighestSortedInteger(List<Integer> sortedList, Integer unsortedElement) {
+        Integer sortedListSize = sortedList.size();
+        for (Integer sortedIndex = 0; sortedIndex < sortedListSize; sortedIndex++) {
+            if (unsortedElement <= sortedList.get(sortedIndex)) {
+                sortedList.add(sortedIndex, unsortedElement);
+                sortedIndex = sortedListSize;
             }
         }
         return sortedList;
     }
+
+    private List addElementToListIfHigherThanBiggestSortedInteger(List<Integer> sortedList, Integer unsortedElement) {
+        Integer sortedListSize = sortedList.size();
+        if (unsortedElement > sortedList.get(sortedListSize - 1)) {
+            sortedList.add(unsortedElement);
+        }
+        return sortedList;
+    }
+
 }
 
 
